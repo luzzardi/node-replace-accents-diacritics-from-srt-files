df6e61fa94142d3321b6901768c2d830ef307886
luzzardi <luzzardi@gmail.com>
2021-06-05 10:48:00 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(`Node.js v${process.versions.node}!`);
 
 const fs = require('fs');
 const readStream = fs.createReadStream('legend.srt');
